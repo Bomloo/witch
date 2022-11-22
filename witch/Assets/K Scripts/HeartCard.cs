689aa6933cc02c95ef472c550fb9cb3e54ea3ebd
kevponce4 <43938961+kevponce4@users.noreply.github.com>
2022-11-21 22:48:29 -0800

    small changes

--- witch/Assets/K Scripts/HeartCard.cs
+++ witch/Assets/K Scripts/HeartCard.cs
@@ -30,28 +30,53 @@ public class HeartCard : Card
         {
             case 1:
                 play.ace_hearts();
+                description = "allows the player to heal when hitting an enemy";
                 break;
             case 5:
                 play.add_health(+30);
+                description = "Increases the players max health by 30";
                 break;
             case 6:
                 play.add_health(+25);
+                description = "Increases the players max health by 25";
                 break;
             case 7:
                 play.add_health(+20);
+                description = "Increases the players max health by 20";
                 break;
             case 8:
                 play.add_health(+15);
+                description = "Increases the players max health by 15";
                 break;
             case 9:
                 play.add_health(+10);
+                description = "Increases the players max health by 10";
                 break;
             case 10:
                 play.add_health(+5);
+                description = "Increases the players max health by 5";
                 break;
 
         }
 
      
+    }
+
+    public override void SetSuitandNumber(string s, int n)
+    {
+        base.SetSuitandNumber(s, n);
+        int i = n;
+        switch (i)
+        {
+            case 4:
+                description = "When used heals the player for a small amount of health";
+                break;
+            case 3:
+                description = "When used heals the player for a medium amount of health";
+                break;
+            case 2:
+                description = "When used heals the player for a large amount of health";
+                break;
+        }
     }
 }
