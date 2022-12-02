83ac5e33381b3b735ce8c20e660cc1fb92152e88
Aaron Lin <aarlin123@gmail.com>
2022-12-01 20:44:16 -0800

    rename smart

--- witch/Assets/K Scripts/SmartMovement.cs
+++ witch/Assets/K Scripts/SmartMovement.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 using UnityEngine.AI;
 
 //[RequireComponent(typeof( NavMeshAgent))]
-public class BasicMovement : MonoBehaviour
+public class SmartMovement : MonoBehaviour
 {
     #region Movement_vars
     public NavMeshAgent Agent;
